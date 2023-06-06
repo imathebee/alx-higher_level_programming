75290551a27b8dec42af473c3be610a00002b485
Imane O <ouiyzmeimane@gmail.com>
2023-06-06 14:43:41 -0700

    added f6 again

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for n1 in range(10):
     for n2 in range(n1 + 1, 10):
-        print("{:d}{:d}".format(n1, n2), end=", " if n1 != 8 or n2 != 9 else "\n")
+        end_str = ", " if n1 != 8 or n2 != 9 else "\n"
+        print("{:d}{:d}".format(n1, n2), end=end_str)
