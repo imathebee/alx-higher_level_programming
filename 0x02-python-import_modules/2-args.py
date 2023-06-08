2d9e5fe5e23bb56d04ffff425a6d124e27fee3db
Imane O <ouiyzmeimane@gmail.com>
2023-06-08 16:25:22 -0700

    added format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,16 +2,16 @@
 import sys
 
 if __name__ == "__main__":
-    """Print the number of and the list of its arguments."""
     argv = sys.argv[1:]  # Exclude the script name from the arguments
 
     num_args = len(argv)
     if num_args == 0:
-        print("0 arguments.")
+        print("{} argument{}.".format(num_args, "s" if num_args != 1 else ""))
     elif num_args == 1:
         print("1 argument:")
-        print("1:", argv[0])
+        print("1: {}".format(argv[0]))
     else:
-        print(num_args, "arguments:")
-        for i in range(num_args):
-            print(i + 1, ":", argv[i])
+        print("{} arguments:".format(num_args))
+        for i, arg in enumerate(argv):
+            print("{}: {}".format(i + 1, arg))
+
