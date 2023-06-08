383a982918d3e854b8d515dd996685fd6eccd7c2
Imane O <ouiyzmeimane@gmail.com>
2023-06-08 16:35:16 -0700

    added 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]  # Exclude the script name from the arguments
+
+    total = 0
+    for arg in argv:
+        total += int(arg)
+
+    print(total)
