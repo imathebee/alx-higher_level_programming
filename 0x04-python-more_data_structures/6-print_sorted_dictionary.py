621d05659008dc8064693cc5696f3bfd466ae390
Imane O <ouiyzmeimane@gmail.com>
2023-06-14 17:40:46 -0700

    added another f6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is None:
+        return
     for key in sorted(a_dictionary.keys()):
-        print(key + ": " + str(a_dictionary[key]))
+        print(key, ": ", a_dictionary[key])
