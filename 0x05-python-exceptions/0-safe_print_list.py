7e549483ec57adc1a72402ddfb53e67beb21a961
Imane O <ouiyzmeimane@gmail.com>
2023-06-28 05:49:30 -0700

    added f0 and remoded space

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0  # Counter variable to keep track of elements printed
-    
+
     try:
         for i in range(x):
             print(my_list[i], end="")
             count += 1
     except IndexError:
         pass  # Ignore index errors if x is greater than the length of my_list
-    
+
     print()  # Print a new line after printing the elements
     return count
