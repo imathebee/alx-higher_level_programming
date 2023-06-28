3eb83da86b4f95cffdbe659b9e75ba40d2ec44b3
Imane O <ouiyzmeimane@gmail.com>
2023-06-27 18:55:10 -0700

    added f3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square. Defaults to 0.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculate and return the area of the square.
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
