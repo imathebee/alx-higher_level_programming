f6f0d97ecabc59e8cdd209e4258317b32dcd5faf
Imane O <ouiyzmeimane@gmail.com>
2023-06-28 12:33:51 -0700

    added f3 edited

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,5 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None  # If b is 0, set result to None
     finally:
-        print("Inside result: {}".format(result))  # Print the result in the finally block
+        print("Inside result: {}".format(result))  # Print res in finally block
     return result
