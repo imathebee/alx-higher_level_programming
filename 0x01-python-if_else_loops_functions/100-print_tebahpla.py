646a60fa6a697e11e25db240726f2d2701297378
Imane O <ouiyzmeimane@gmail.com>
2023-07-02 09:44:24 -0700

    adde

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for c in range(ord('z'), ord('A') - 1, -1):
-    print(chr(c) if (ord('z') - c) % 2 == 0 else chr(c - 32), end='')
+    print("".join(chr(c) if (ord('z') - c) % 2 == 0 else chr(c - 32) for c in range(ord('z'), ord('A') - 1, -1)), end='')
