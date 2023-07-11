300d2bae995f4724dcbd469a06bbd1c8ea63bd63
Imane O <ouiyzmeimane@gmail.com>
2023-07-10 19:32:29 -0700

    added f11e

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,7 +7,9 @@ class Square(Rectangle):
     """class body."""
 
     def __init__(self, size):
-
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
