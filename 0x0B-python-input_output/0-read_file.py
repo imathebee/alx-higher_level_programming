c130db91ef6d5231037da96fdb644489b33d98fb
Imane O <ouiyzmeimane@gmail.com>
2023-07-12 15:43:45 -0700

    added file0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""Defines function read_file"""
+
+
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints its content to stdout."""
-    with open(filename, encoding="utf-8") as file:
+    """
+    A function that reads the contents of a file
+
+    Args:
+        filename(str):The name or path of the file to read.
+
+    Returns:
+        None
+    """
+    with open(filename, mode='r', encoding='utf-8') as file:
         for line in file:
             print(line, end="")
